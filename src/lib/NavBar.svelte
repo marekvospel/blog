e9665d237a4f972557dee4b58782434dabb54dcc
Marek Vospěl <marek@vospel.cz>
2023-01-17 20:38:14 +0100

    feat: update navbar to match vospel.cz

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,23 +1,26 @@
 <script>
   import MdiGithub from '~icons/mdi/github'
+  import MdiMastodon from '~icons/mdi/mastodon'
   import MdiTwitter from '~icons/mdi/twitter'
-
 </script>
 
 <div class="flex flex-row py-2 lg-px4">
-  <a href="https://vospel.cz" class="group">
-    <span class="text-cyan-400 group-hover:text-cyan-200 group-focus:text-cyan-200 transition-color duration-300">Blog</span> | Marek Vospěl
+  <a href="/" class="group">
+    <span class="text-cyan-400 group-hover:text-cyan-200 group-focus:text-cyan-200 transition-color duration-300">Marek Vospěl</span>
   </a>
 
   <div class="flex flex-row items-center gap-2 ml-auto">
-    <a href="/" class="text-gray-500 hover:text-cyan-400 focus-visible:text-cyan-400">Blog</a>
+    <a href="https://blog.vospel.cz" class="text-gray-500 hover:text-cyan-400 focus-visible:text-cyan-400">Blog</a>
     <br />
-    <a href="https://github.com/marekvospel/blog" target="_blank" rel="noopener noreferrer" class="text-2xl group">
+    <a href="https://github.com/marekvospel" target="_blank" rel="noopener noreferrer me" class="text-2xl group">
       <MdiGithub class="text-gray-500 hover:text-cyan-400 group-focus:text-cyan-400 transition-color duration-300"/>
     </a>
-    <a href="https://twitter.com/marekvospel" target="_blank" rel="noopener noreferrer" class="text-2xl group">
+    <a href="https://cyberplace.social/@vospel" target="_blank" rel="noopener noreferrer me" class="text-2xl group">
+      <MdiMastodon class="text-gray-500 hover:text-cyan-400 group-focus:text-cyan-400 transition-color duration-300"/>
+    </a>
+    <a href="https://twitter.com/marekvospel" target="_blank" rel="noopener noreferrer me" class="text-2xl group">
       <MdiTwitter class="text-gray-500 hover:text-cyan-400 group-focus:text-cyan-400 transition-color duration-300"/>
     </a>
   </div>
-  
+
 </div>
